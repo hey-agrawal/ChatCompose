d38845c6096e9d46cd95aef2898a3be98aaeca3c
Kofi K <kofivincent026@gmail.com>
2022-12-29 02:33:20 +0000

    Material 3 dependency added Roboto typography added Colorscheme for purple added

--- app/src/main/java/com/example/chatcompose/ui/theme/Type.kt
+++ app/src/main/java/com/example/chatcompose/ui/theme/Type.kt
@@ -1,28 +1,125 @@
 package com.example.chatcompose.ui.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.chatcompose.R
 
+
+val RobotoFont = FontFamily(
+    Font(resId = R.font.roboto_regular),
+    Font(resId = R.font.roboto_medium),
+    Font(resId = R.font.roboto_bold),
+)
 // Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+
+val ChatComposeTypography = Typography(
+    labelLarge = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.sp,
+        lineHeight = 20.sp,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    labelMedium = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.10000000149011612.sp,
+        lineHeight = 16.sp,
         fontSize = 12.sp
-    )
-    */
+    ),
+    labelSmall = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.10000000149011612.sp,
+        lineHeight = 16.sp,
+        fontSize = 11.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.10000000149011612.sp,
+        lineHeight = 16.sp,
+        fontSize = 12.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 40.sp,
+        fontSize = 32.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 36.sp,
+        fontSize = 28.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 32.sp,
+        fontSize = 24.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 64.sp,
+        fontSize = 57.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 52.sp,
+        fontSize = 45.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 44.sp,
+        fontSize = 36.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.W400,
+        letterSpacing = 0.sp,
+        lineHeight = 28.sp,
+        fontSize = 22.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.sp,
+        lineHeight = 24.sp,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = RobotoFont,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.sp,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
+    ),
 )
